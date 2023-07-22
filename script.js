09a683a66dd27ab02f27e9afa2b2a2f99770ba53
saramazaheri <61461140+saramazaheri@users.noreply.github.com>
2023-07-22 23:08:37 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-import sum, { name, sayHi } from "./app.js";
-console.log(name);
-sayHi();
-console.log(sum(2, 5));
+// import sum, { name, sayHi } from "./app.js";
+// console.log(name);
+// sayHi();
+// console.log(sum(2, 5));
+
+class vehicle {
+  constructor(color, model) {
+    this.color = color;
+    this.model = model;
+  }
+  speed() {
+    console.log(this);
+    console.log("I go");
+  }
+}
+
+class car extends vehicle {
+  constructor(color, model, year) {
+    super(color, model);
+    this.year = year;
+  }
+}
+const vehicle1 = new vehicle("red", "bmw");
+console.log(vehicle1);
+
+const car1 = new car("pink", "fulex", 2023);
+console.log(car1);
