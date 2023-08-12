6aac6b3f905e76dc439e1af8697e38c95ff6457b
saramazaheri <61461140+saramazaheri@users.noreply.github.com>
2023-08-12 19:58:48 +0330

    Update App.jsx

--- test-app/src/App.jsx
+++ test-app/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Benz",
+      speed: "250",
+    };
+  }
 
-  return (
-    <>
+  ChangeHandler = () => {
+    this.setState({
+      name: "BMW",
+    });
+  };
+
+  render() {
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          My first car is {this.state.name} and it has {this.state.speed} speed.
         </p>
+        <button onClick={this.ChangeHandler}>Change</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
-
-export default App
+export default App;
