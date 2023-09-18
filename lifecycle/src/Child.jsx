41e8ad2744291f7a8fc0ca013d7ccf1029620d7f
saramazaheri <61461140+saramazaheri@users.noreply.github.com>
2023-09-18 16:58:02 +0330

    Update Child.jsx

--- lifecycle/src/Child.jsx
+++ lifecycle/src/Child.jsx
@@ -1,9 +1,45 @@
-import React, { PureComponent } from "react";
+//Class Components
+// import React, { PureComponent } from "react";
 
-class Child extends PureComponent {
+// class Child extends PureComponent {
+//   render() {
+//     console.log("Child is rendered");
+//     return <div>{this.props.text} Child</div>;
+//   }
+// }
+
+// export default Child;
+
+//Functional Components
+// import React from "react";
+
+// const Child = (props) => {
+//   console.log("Child is rendered");
+//   return <div>{props.text} Child</div>;
+// };
+
+// export default React.memo(Child);
+
+import React, { Component } from "react";
+
+class Child extends Component {
+  constructor(props) {
+    super(props);
+    this.input = React.createRef();
+  }
+  Showref = () => {
+    console.log(this.input.current.value);
+  };
+  componentDidMount() {
+    this.input.current.focus();
+  }
   render() {
-    console.log("Child is rendered");
-    return <div>{this.props.text} Child</div>;
+    return (
+      <div>
+        <button onClick={this.Showref}>Show</button>
+        <input ref={this.input} type="text" />
+      </div>
+    );
   }
 }
 
