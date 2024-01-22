5ac0373f0ad5c67396801f6b2cc94541f6599060
saramazaheri <61461140+saramazaheri@users.noreply.github.com>
2024-01-22 14:00:30 +0330

    Update Counter.jsx

--- new-app/first-app/src/Counter.jsx
+++ new-app/first-app/src/Counter.jsx
@@ -3,12 +3,18 @@ import { useState } from "react";
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
-  const increaseHandler = () => setCounter(counter + 1);
+  const increaseHandler = () => setCounter((counter) => counter + 1);
+  const increaseHandler3 = () => setCounter((counter) => counter + 3);
+  const stopHandler = () => setCounter("-");
+  const decreaseHandler = () => setCounter((counter) => counter - 1);
   return (
     <div>
       <h1>Counter</h1>
       <p>{counter}</p>
-      <button onClick={increaseHandler}>Increase</button>
+      <button onClick={increaseHandler}>Increase1</button>
+      <button onClick={increaseHandler3}>Increase3</button>
+      <button onClick={stopHandler}>Stop</button>
+      <button onClick={decreaseHandler}>Decrease</button>
     </div>
   );
 };
