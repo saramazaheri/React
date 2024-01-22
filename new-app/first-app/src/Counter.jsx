f9d00986dd259eaf1283a1a3e9b48536403819a1
saramazaheri <61461140+saramazaheri@users.noreply.github.com>
2024-01-22 13:37:42 +0330

    Update Counter.jsx

--- new-app/first-app/src/Counter.jsx
+++ new-app/first-app/src/Counter.jsx
@@ -1,13 +1,16 @@
-function Counter() {
-  function increaseHandler() {
-    console.log("Increase!");
-  }
+import { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseHandler = () => setCounter(counter + 1);
   return (
     <div>
       <h1>Counter</h1>
+      <p>{counter}</p>
       <button onClick={increaseHandler}>Increase</button>
     </div>
   );
-}
+};
 
 export default Counter;
